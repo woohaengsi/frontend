749ff1e909ff1ed651aa37dc192404c626bcd475
greathoaxer <shorelinesquare@naver.com>
2023-07-06 00:15:17 +0900

    :tada: Begin a new project
    기존에 짜여져 있는 코드를 좀 더 간결하게 작성하고자 합니다. 로그도 덤으로 남기구요.

---
+++ MyGrowthDiary/MyGrowthDiary/MyGrowthDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyGrowthDiaryApp.swift
+//  MyGrowthDiary
+//
+//  Created by Hyung Seo Han on 2023/07/06.
+//
+
+import SwiftUI
+
+@main
+struct MyGrowthDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
